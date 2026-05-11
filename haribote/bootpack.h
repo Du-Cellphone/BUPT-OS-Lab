c87e93aaa7811190cdbc4708dab590657c3f97e4
7735H <dxf20050307@gmail.com>
2026-05-11 23:08:35 +0800

    -merge Multi Task Schedule

--- haribote/bootpack.h
+++ haribote/bootpack.h
@@ -1,55 +1,57 @@
 /* asmhead.nas */
 struct BOOTINFO
-{				/* 0x0ff0-0x0fff */
-	char cyls;	/* ブートセクタはどこまでディスクを読んだのか */
-	char leds;	/* ブート時のキーボードのLEDの状態 */
-	char vmode; /* ビデオモード  何ビットカラーか */
-	char reserve;
-	short scrnx, scrny; /* 画面解像度 */
-	char *vram;
+{                /* 0x0ff0-0x0fff */
+    char  cyls;  /* ブートセクタはどこまでディスクを読んだのか */
+    char  leds;  /* ブート時のキーボードのLEDの状態 */
+    char  vmode; /* ビデオモード  何ビットカラーか */
+    char  reserve;
+    short scrnx, scrny; /* 画面解像度 */
+    char *vram;
 };
 #define ADR_BOOTINFO 0x00000ff0
 #define ADR_DISKIMG 0x00100000
 
+extern int preempt_needed;
+
 /* naskfunc.nas */
-void io_hlt(void);
-void io_cli(void);
-void io_sti(void);
-void io_stihlt(void);
-int io_in8(int port);
-void io_out8(int port, int data);
-int io_load_eflags(void);
-void io_store_eflags(int eflags);
-void load_gdtr(int limit, int addr);
-void load_idtr(int limit, int addr);
-int load_cr0(void);
-void store_cr0(int cr0);
-void store_cr3(int cr3);
-void switch_to_high_half(void);
-void load_tr(int tr);
-void asm_inthandler0c(void);
-void asm_inthandler0d(void);
-void asm_inthandler20(void);
-void asm_inthandler21(void);
-void asm_inthandler2c(void);
+void         io_hlt(void);
+void         io_cli(void);
+void         io_sti(void);
+void         io_stihlt(void);
+int          io_in8(int port);
+void         io_out8(int port, int data);
+int          io_load_eflags(void);
+void         io_store_eflags(int eflags);
+void         load_gdtr(int limit, int addr);
+void         load_idtr(int limit, int addr);
+int          load_cr0(void);
+void         store_cr0(int cr0);
+void         store_cr3(int cr3);
+void         switch_to_high_half(void);
+void         load_tr(int tr);
+void         asm_inthandler0c(void);
+void         asm_inthandler0d(void);
+void         asm_inthandler20(void);
+void         asm_inthandler21(void);
+void         asm_inthandler2c(void);
 unsigned int memtest_sub(unsigned int start, unsigned int end);
-void farjmp(int eip, int cs);
-void farcall(int eip, int cs);
-void asm_hrb_api(void);
-void start_app(int eip, int cs, int esp, int ds, int *tss_esp0);
-void asm_end_app(void);
+void         farjmp(int eip, int cs);
+void         farcall(int eip, int cs);
+void         asm_hrb_api(void);
+void         start_app(int eip, int cs, int esp, int ds, int *tss_esp0);
+void         asm_end_app(void);
 
 /* fifo.c */
 struct FIFO32
 {
-	int *buf;
-	int p, q, size, free, flags;
-	struct TASK *task;
+    int         *buf;
+    int          p, q, size, free, flags;
+    struct TASK *task;
 };
 void fifo32_init(struct FIFO32 *fifo, int size, int *buf, struct TASK *task);
-int fifo32_put(struct FIFO32 *fifo, int data);
-int fifo32_get(struct FIFO32 *fifo);
-int fifo32_status(struct FIFO32 *fifo);
+int  fifo32_put(struct FIFO32 *fifo, int data);
+int  fifo32_get(struct FIFO32 *fifo);
+int  fifo32_status(struct FIFO32 *fifo);
 
 /* graphic.c */
 void init_palette(void);
@@ -59,8 +61,7 @@ void init_screen8(char *vram, int x, int y);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
 void init_mouse_cursor8(char *mouse, char bc);
-void putblock8_8(char *vram, int vxsize, int pxsize,
-				 int pysize, int px0, int py0, char *buf, int bxsize);
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
 #define COL8_000000 0
 #define COL8_FF0000 1
 #define COL8_00FF00 2
@@ -81,15 +82,15 @@ void putblock8_8(char *vram, int vxsize, int pxsize,
 /* dsctbl.c */
 struct SEGMENT_DESCRIPTOR
 {
-	short limit_low, base_low;
-	char base_mid, access_right;
-	char limit_high, base_high;
+    short limit_low, base_low;
+    char  base_mid, access_right;
+    char  limit_high, base_high;
 };
 struct GATE_DESCRIPTOR
 {
-	short offset_low, selector;
-	char dw_count, access_right;
-	short offset_high;
+    short offset_low, selector;
+    char  dw_count, access_right;
+    short offset_high;
 };
 void init_gdtidt(void);
 void init_gdt_high(void);
@@ -132,12 +133,12 @@ void init_keyboard(struct FIFO32 *fifo, int data0);
 /* mouse.c */
 struct MOUSE_DEC
 {
-	unsigned char buf[3], phase;
-	int x, y, btn;
+    unsigned char buf[3], phase;
+    int           x, y, btn;
 };
 void inthandler2c(int *esp);
 void enable_mouse(struct FIFO32 *fifo, int data0, struct MOUSE_DEC *mdec);
-int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
+int  mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 
 /* memory.c */
 #define MEMMAN_FREES 4090 /* これで約32KB */
@@ -147,119 +148,119 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 #define KERNEL_BASE 0xC0000000
 struct FREEINFO
 { /* あき情報 */
-	unsigned int addr, size;
+    unsigned int addr, size;
 };
 struct MEMMAN
 { /* メモリ管理 */
-	int frees, maxfrees, lostsize, losts;
-	struct FREEINFO free[MEMMAN_FREES];
+    int             frees, maxfrees, lostsize, losts;
+    struct FREEINFO free[MEMMAN_FREES];
 };
 unsigned int memtest(unsigned int start, unsigned int end);
 unsigned int init_paging(unsigned int memtotal);
-void memman_init(struct MEMMAN *man);
+void         memman_init(struct MEMMAN *man);
 unsigned int memman_total(struct MEMMAN *man);
 unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
-int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+int          memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
 unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
-int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
+int          memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
 /* sheet.c */
 #define MAX_SHEETS 256
 struct SHEET
 {
-	unsigned char *buf;
-	int bxsize, bysize, vx0, vy0, col_inv, height, flags;
-	struct SHTCTL *ctl;
-	struct TASK *task;
+    unsigned char *buf;
+    int            bxsize, bysize, vx0, vy0, col_inv, height, flags;
+    struct SHTCTL *ctl;
+    struct TASK   *task;
 };
 struct SHTCTL
 {
-	unsigned char *vram, *map;
-	int xsize, ysize, top;
-	struct SHEET *sheets[MAX_SHEETS];
-	struct SHEET sheets0[MAX_SHEETS];
+    unsigned char *vram, *map;
+    int            xsize, ysize, top;
+    struct SHEET  *sheets[MAX_SHEETS];
+    struct SHEET   sheets0[MAX_SHEETS];
 };
 struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize, int ysize);
-struct SHEET *sheet_alloc(struct SHTCTL *ctl);
-void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
-void sheet_updown(struct SHEET *sht, int height);
-void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1);
-void sheet_slide(struct SHEET *sht, int vx0, int vy0);
-void sheet_free(struct SHEET *sht);
+struct SHEET  *sheet_alloc(struct SHTCTL *ctl);
+void           sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
+void           sheet_updown(struct SHEET *sht, int height);
+void           sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1);
+void           sheet_slide(struct SHEET *sht, int vx0, int vy0);
+void           sheet_free(struct SHEET *sht);
 
 /* timer.c */
 #define MAX_TIMER 500
 struct TIMER
 {
-	struct TIMER *next;
-	unsigned int timeout;
-	char flags, flags2;
-	struct FIFO32 *fifo;
-	int data;
+    struct TIMER  *next;
+    unsigned int   timeout;
+    char           flags, flags2;
+    struct FIFO32 *fifo;
+    int            data;
 };
 struct TIMERCTL
 {
-	unsigned int count, next;
-	struct TIMER *t0;
-	struct TIMER timers0[MAX_TIMER];
+    unsigned int  count, next;
+    struct TIMER *t0;
+    struct TIMER  timers0[MAX_TIMER];
 };
 extern struct TIMERCTL timerctl;
-void init_pit(void);
-struct TIMER *timer_alloc(void);
-void timer_free(struct TIMER *timer);
-void timer_init(struct TIMER *timer, struct FIFO32 *fifo, int data);
-void timer_settime(struct TIMER *timer, unsigned int timeout);
-void inthandler20(int *esp);
-int timer_cancel(struct TIMER *timer);
-void timer_cancelall(struct FIFO32 *fifo);
+void                   init_pit(void);
+struct TIMER          *timer_alloc(void);
+void                   timer_free(struct TIMER *timer);
+void                   timer_init(struct TIMER *timer, struct FIFO32 *fifo, int data);
+void                   timer_settime(struct TIMER *timer, unsigned int timeout);
+void                   inthandler20(int *esp);
+int                    timer_cancel(struct TIMER *timer);
+void                   timer_cancelall(struct FIFO32 *fifo);
 
 /* mtask.c */
 #define MAX_TASKS 1000 /* 最大タスク数 */
-#define TASK_GDT0 3	   /* TSSをGDTの何番から割り当てるのか */
+#define TASK_GDT0 3    /* TSSをGDTの何番から割り当てるのか */
 #define MAX_TASKS_LV 100
 #define MAX_TASKLEVELS 10
 struct TSS32
 {
-	int backlink, esp0, ss0, esp1, ss1, esp2, ss2, cr3;
-	int eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
-	int es, cs, ss, ds, fs, gs;
-	int ldtr, iomap;
+    int backlink, esp0, ss0, esp1, ss1, esp2, ss2, cr3;
+    int eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    int es, cs, ss, ds, fs, gs;
+    int ldtr, iomap;
 };
 struct TASK
 {
-	int sel, flags; /* selはGDTの番号のこと */
-	int level, priority;
-	struct FIFO32 fifo;
-	struct TSS32 tss;
-	struct SEGMENT_DESCRIPTOR ldt[2];
-	struct CONSOLE *cons;
-	int ds_base, cons_stack;
-	struct FILEHANDLE *fhandle;
-	int *fat;
-	char *cmdline;
-	unsigned char langmode, langbyte1;
+    int                       sel, flags; /* selはGDTの番号のこと */
+    int                       level, priority;
+    struct FIFO32             fifo;
+    struct TSS32              tss;
+    struct SEGMENT_DESCRIPTOR ldt[2];
+    struct CONSOLE           *cons;
+    int                       ds_base, cons_stack;
+    struct FILEHANDLE        *fhandle;
+    int                      *fat;
+    char                     *cmdline;
+    unsigned char             langmode, langbyte1;
 };
 struct TASKLEVEL
 {
-	int running; /* 動作しているタスクの数 */
-	int now;	 /* 現在動作しているタスクがどれだか分かるようにするための変数 */
-	struct TASK *tasks[MAX_TASKS_LV];
+    int          running; /* 動作しているタスクの数 */
+    int          now;     /* 現在動作しているタスクがどれだか分かるようにするための変数 */
+    struct TASK *tasks[MAX_TASKS_LV];
 };
 struct TASKCTL
 {
-	int now_lv;		/* 現在動作中のレベル */
-	char lv_change; /* 次回タスクスイッチのときに、レベルも変えたほうがいいかどうか */
-	struct TASKLEVEL level[MAX_TASKLEVELS];
-	struct TASK tasks0[MAX_TASKS];
+    int              now_lv;    /* 現在動作中のレベル */
+    char             lv_change; /* 次回タスクスイッチのときに、レベルも変えたほうがいいかどうか */
+    struct TASKLEVEL level[MAX_TASKLEVELS];
+    struct TASK      tasks0[MAX_TASKS];
 };
 extern struct TASKCTL *taskctl;
-extern struct TIMER *task_timer;
-struct TASK *task_now(void);
-struct TASK *task_init(struct MEMMAN *memman);
-struct TASK *task_alloc(void);
-void task_run(struct TASK *task, int level, int priority);
-void task_switch(void);
-void task_sleep(struct TASK *task);
+extern struct TIMER   *task_timer;
+struct TASK           *task_now(void);
+struct TASK           *task_init(struct MEMMAN *memman);
+struct TASK           *task_alloc(void);
+void                   task_run(struct TASK *task, int level, int priority);
+void                   task_switch(void);
+void                   task_sleep(struct TASK *task);
 
 /* window.c */
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
@@ -271,15 +272,15 @@ void change_wtitle8(struct SHEET *sht, char act);
 /* console.c */
 struct CONSOLE
 {
-	struct SHEET *sht;
-	int cur_x, cur_y, cur_c;
-	struct TIMER *timer;
+    struct SHEET *sht;
+    int           cur_x, cur_y, cur_c;
+    struct TIMER *timer;
 };
 struct FILEHANDLE
 {
-	char *buf;
-	int size;
-	int pos;
+    char *buf;
+    int   size;
+    int   pos;
 };
 void console_task(struct SHEET *sheet, int memtotal);
 void cons_putchar(struct CONSOLE *cons, int chr, char move);
@@ -294,7 +295,7 @@ void cmd_exit(struct CONSOLE *cons, int *fat);
 void cmd_start(struct CONSOLE *cons, char *cmdline, int memtotal);
 void cmd_ncst(struct CONSOLE *cons, char *cmdline, int memtotal);
 void cmd_langmode(struct CONSOLE *cons, char *cmdline);
-int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline);
+int  cmd_app(struct CONSOLE *cons, int *fat, char *cmdline);
 int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax);
 int *inthandler0d(int *esp);
 int *inthandler0c(int *esp);
@@ -303,20 +304,20 @@ void hrb_api_linewin(struct SHEET *sht, int x0, int y0, int x1, int y1, int col)
 /* file.c */
 struct FILEINFO
 {
-	unsigned char name[8], ext[3], type;
-	char reserve[10];
-	unsigned short time, date, clustno;
-	unsigned int size;
+    unsigned char  name[8], ext[3], type;
+    char           reserve[10];
+    unsigned short time, date, clustno;
+    unsigned int   size;
 };
-void file_readfat(int *fat, unsigned char *img);
-void file_loadfile(int clustno, int size, char *buf, int *fat, char *img);
+void             file_readfat(int *fat, unsigned char *img);
+void             file_loadfile(int clustno, int size, char *buf, int *fat, char *img);
 struct FILEINFO *file_search(char *name, struct FILEINFO *finfo, int max);
-char *file_loadfile2(int clustno, int *psize, int *fat);
+char            *file_loadfile2(int clustno, int *psize, int *fat);
 
 /* tek.c */
 int tek_getsize(unsigned char *p);
 int tek_decomp(unsigned char *p, char *q, int size);
 
 /* bootpack.c */
-struct TASK *open_constask(struct SHEET *sht, unsigned int memtotal);
+struct TASK  *open_constask(struct SHEET *sht, unsigned int memtotal);
 struct SHEET *open_console(struct SHTCTL *shtctl, unsigned int memtotal);
